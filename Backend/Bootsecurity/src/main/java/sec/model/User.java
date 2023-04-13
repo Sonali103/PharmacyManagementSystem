4d518df2fa537a621f4c51bda82303dc3e0d8dab
Sonali103 <115690703+Sonali103@users.noreply.github.com>
2023-04-13 15:41:28 +0530

    PMS

--- Backend/Bootsecurity/src/main/java/sec/model/User.java
+++ Backend/Bootsecurity/src/main/java/sec/model/User.java
@@ -2,43 +2,126 @@ package sec.model;
 
 import java.util.HashSet;
 import java.util.Set;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 @Document(collection = "users")
 public class User {
   @Id
   private String userId;
+
   @NotBlank
   @Size(max = 20)
   private String username;
+
   @NotBlank
   @Size(max = 50)
   @Email
   private String email;
+
   @NotBlank
   @Size(max = 120)
   private String password;
+
   @DBRef
   private Set<Role> roles = new HashSet<>();
-  
+
   public User(String userId, String username, String email, String password) {
-	  	this.userId = userId;
-	    this.username = username;
-	    this.email = email;
-	    this.password = password;
-	  }
+
+    this.userId = userId;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+
+  /**
+   * @return userId
+   */
+  public String getUserId() {
+
+    return this.userId;
+  }
+
+  /**
+   * @param userId new value of {@link #getuserId}.
+   */
+  public void setUserId(String userId) {
+
+    this.userId = userId;
+  }
+
+  /**
+   * @return username
+   */
+  public String getUsername() {
+
+    return this.username;
+  }
+
+  /**
+   * @param username new value of {@link #getusername}.
+   */
+  public void setUsername(String username) {
+
+    this.username = username;
+  }
+
+  /**
+   * @return email
+   */
+  public String getEmail() {
+
+    return this.email;
+  }
+
+  /**
+   * @param email new value of {@link #getemail}.
+   */
+  public void setEmail(String email) {
+
+    this.email = email;
+  }
+
+  /**
+   * @return password
+   */
+  public String getPassword() {
+
+    return this.password;
+  }
+
+  /**
+   * @param password new value of {@link #getpassword}.
+   */
+  public void setPassword(String password) {
+
+    this.password = password;
+  }
+
+  /**
+   * @return roles
+   */
+  public Set<Role> getRoles() {
+
+    return this.roles;
+  }
+
+  /**
+   * @param roles new value of {@link #getroles}.
+   */
+  public void setRoles(Set<Role> roles) {
+
+    this.roles = roles;
+  }
 
 }
