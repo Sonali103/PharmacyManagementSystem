09da6fb191674c26c2c7245d2fe570dd78a6a7a7
Sonali103 <115690703+Sonali103@users.noreply.github.com>
2023-04-13 16:13:00 +0530

    PMS

--- Backend/Drugs-Microservice/src/main/java/com/drugs/models/Inventory.java
+++ Backend/Drugs-Microservice/src/main/java/com/drugs/models/Inventory.java
@@ -2,16 +2,45 @@ package com.drugs.models;
 
 import java.util.List;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Inventory {
-	
-	private List<Drugs> drugs;
-	private Supplier supplier;
+
+  private List<Drugs> drugs;
+
+  private Supplier supplier;
+
+  /**
+   * @return drugs
+   */
+  public List<Drugs> getDrugs() {
+
+    return this.drugs;
+  }
+
+  /**
+   * @param drugs new value of {@link #getdrugs}.
+   */
+  public void setDrugs(List<Drugs> drugs) {
+
+    this.drugs = drugs;
+  }
+
+  /**
+   * @return supplier
+   */
+  public Supplier getSupplier() {
+
+    return this.supplier;
+  }
+
+  /**
+   * @param supplier new value of {@link #getsupplier}.
+   */
+  public void setSupplier(Supplier supplier) {
+
+    this.supplier = supplier;
+  }
 
 }
