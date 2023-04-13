dfe7891e609c2c3fd22c4a9442c4ea2c53da312a
Sonali103 <115690703+Sonali103@users.noreply.github.com>
2023-04-13 16:20:22 +0530

    PMS

--- Backend/Drugs-Microservice/src/main/java/com/drugs/service/DrugsService.java
+++ Backend/Drugs-Microservice/src/main/java/com/drugs/service/DrugsService.java
@@ -9,7 +9,6 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,142 +17,140 @@ import com.drugs.exception.CustomException;
 import com.drugs.models.Cart;
 import com.drugs.models.Drugs;
 import com.drugs.models.Inventory;
-import com.drugs.models.MessageResponse;
 import com.drugs.models.Photo;
 import com.drugs.models.Supplier;
 import com.drugs.repository.DrugsRepository;
 
 @Service
 public class DrugsService {
-	
-	@Autowired
-	private DrugsRepository drugsRepository;
-	
-	@Autowired
-	private SequenceGeneratorService sequenceGeneratorService;
-	
-	@Autowired
-	private RestTemplate restTemplate;
-	
-	private static final Logger LOGGER=LoggerFactory.getLogger(DrugsService.class);
 
-	public Drugs saveDrugsDetails(Drugs drugs, MultipartFile[] file) throws CustomException, Exception{
-		 Set<Photo> images = uploadImage(file);
-			drugs.setProductImages(images);
-			LOGGER.info("Sucessfully Registered new Drug");
-		drugs.setDrugsId(String.valueOf(sequenceGeneratorService.generateSequence(drugs.SEQUENCE_NAME)));
-		Drugs save = this.drugsRepository.save(drugs);
-		
-		return save;
-		
-	}
-	
-	public Set<Photo> uploadImage(MultipartFile[] multipartFiles) throws IOException{
-		Set<Photo> imageModels = new HashSet<>();
-		
-		for(MultipartFile file: multipartFiles) {
-			Photo imageModels1 = new Photo(
-					file.getOriginalFilename(),
-					file.getContentType(),
-					file.getBytes()
-					);
-			imageModels.add(imageModels1);
-		}
-		return imageModels;
-	}
+  @Autowired
+  private DrugsRepository drugsRepository;
 
-	public List<Drugs> getAllDetails() {
-		return drugsRepository.findAll();
-	}
+  @Autowired
+  private SequenceGeneratorService sequenceGeneratorService;
 
-	public Optional<Drugs> getDrugsByID(String drugsId) throws CustomException, Exception{
-		Optional<Drugs> drugs = Optional.empty();
-		try {
-			if(drugsRepository.existsByDrugsId(drugsId)) {
-				drugs = drugsRepository.findById(drugsId);
-			}else {
-				throw new CustomException("The mentioned ID is not listed");
-			}
-		} catch(CustomException e) {
-			LOGGER.error("The mentioned ID is not listed"+e);
-		}
-		return drugs;
-	}
-	
-	public List<Drugs> getDrugsBySupplierName(String supplierName){
-		return drugsRepository.findBySupplierName(supplierName);
-	}
-	
-	public List<Drugs> getDrugsByName(String drugsName){
-		return drugsRepository.findByDrugsName(drugsName);
-	}
-	
-	public List<Drugs> getDrugsByCategory(String categories){
-		return drugsRepository.findBycategories(categories);
-	}
-	
-	
-	public Inventory getDrugsWithSupplierDetails(String supplierName) throws CustomException, Exception {
-		String name = null;
-		Inventory response = new Inventory();
-		try {
-			if(drugsRepository.existsBySupplierName(supplierName)) {
-				List<Drugs> drugs = drugsRepository.findBySupplierName(supplierName);
-				for(Drugs var : drugs) {
-					 name = var.getSupplierName();
-				}
-				
-			
-				
-				Supplier supplier = restTemplate.getForObject("http://SUPPLIER-SERVICE/supplier/details/"+ name, Supplier.class);
-				response.setDrugs(drugs);
-				response.setSupplier(supplier);
-			}else {
-				LOGGER.error("The result could not be fetched");
-				throw new CustomException("The supplierName couldn't be fetched");
-			}
-		} catch(CustomException e) {
-			LOGGER.error("The supplierName couldn't be fetched"+e);
-		}
-		
-		return response;
-				
-		
-	}
-	
-	
-	public Drugs updateDrugsByCart(String username, String drugsId, Drugs drugs) {
-		
-		Cart cart = restTemplate.getForObject("http://DRUGS-MICROSERVICE/drugs/cart/username/"+drugsId+"/"+username, Cart.class);
-		int x = cart.getDrugsQty();
-		
-		Drugs drugs1 = restTemplate.getForObject("http://DRUGS-MICROSERVICE/drugs/"+drugsId,Drugs.class);
-		int y = drugs1.getStockQty();
-		
-		int z = y - x;
-		
-		drugs.setStockQty(z);
-		
-		drugsRepository.save(drugs);
-		
-		return drugs;
-		
-		
-	}
-	
-	
-	public Drugs updateDrugsDetails(Drugs drugs, String drugsId) throws CustomException, Exception{
-		Drugs storingVar = new Drugs();
-			storingVar = drugsRepository.save(drugs);
-		
-		return storingVar;
-	}
-	
+  @Autowired
+  private RestTemplate restTemplate;
 
-	public String deleteDrugs(String drugsId) {
-		drugsRepository.deleteById(drugsId);
-		return "Deleted Successfully";
-	}
-	
+  private static final Logger LOGGER = LoggerFactory.getLogger(DrugsService.class);
+
+  public Drugs saveDrugsDetails(Drugs drugs, MultipartFile[] file) throws CustomException, Exception {
+
+    Set<Photo> images = uploadImage(file);
+    drugs.setProductImages(images);
+    LOGGER.info("Sucessfully Registered new Drug");
+    drugs.setDrugsId(String.valueOf(this.sequenceGeneratorService.generateSequence(Drugs.SEQUENCE_NAME)));
+    Drugs save = this.drugsRepository.save(drugs);
+
+    return save;
+
+  }
+
+  public Set<Photo> uploadImage(MultipartFile[] multipartFiles) throws IOException {
+
+    Set<Photo> imageModels = new HashSet<>();
+
+    for (MultipartFile file : multipartFiles) {
+      Photo imageModels1 = new Photo(file.getOriginalFilename(), file.getContentType(), file.getBytes());
+      imageModels.add(imageModels1);
+    }
+    return imageModels;
+  }
+
+  public List<Drugs> getAllDetails() {
+
+    return this.drugsRepository.findAll();
+  }
+
+  public Optional<Drugs> getDrugsByID(String drugsId) throws CustomException, Exception {
+
+    Optional<Drugs> drugs = Optional.empty();
+    try {
+      if (this.drugsRepository.existsByDrugsId(drugsId)) {
+        drugs = this.drugsRepository.findById(drugsId);
+      } else {
+        throw new CustomException("The mentioned ID is not listed");
+      }
+    } catch (CustomException e) {
+      LOGGER.error("The mentioned ID is not listed" + e);
+    }
+    return drugs;
+  }
+
+  public List<Drugs> getDrugsBySupplierName(String supplierName) {
+
+    return this.drugsRepository.findBySupplierName(supplierName);
+  }
+
+  public List<Drugs> getDrugsByName(String drugsName) {
+
+    return this.drugsRepository.findByDrugsName(drugsName);
+  }
+
+  public List<Drugs> getDrugsByCategory(String categories) {
+
+    return this.drugsRepository.findBycategories(categories);
+  }
+
+  public Inventory getDrugsWithSupplierDetails(String supplierName) throws CustomException, Exception {
+
+    String name = null;
+    Inventory response = new Inventory();
+    try {
+      if (this.drugsRepository.existsBySupplierName(supplierName)) {
+        List<Drugs> drugs = this.drugsRepository.findBySupplierName(supplierName);
+        for (Drugs var : drugs) {
+          name = var.getSupplierName();
+        }
+
+        Supplier supplier = this.restTemplate.getForObject("http://SUPPLIER-SERVICE/supplier/details/" + name,
+            Supplier.class);
+        response.setDrugs(drugs);
+        response.setSupplier(supplier);
+      } else {
+        LOGGER.error("The result could not be fetched");
+        throw new CustomException("The supplierName couldn't be fetched");
+      }
+    } catch (CustomException e) {
+      LOGGER.error("The supplierName couldn't be fetched" + e);
+    }
+
+    return response;
+
+  }
+
+  public Drugs updateDrugsByCart(String username, String drugsId, Drugs drugs) {
+
+    Cart cart = this.restTemplate
+        .getForObject("http://DRUGS-MICROSERVICE/drugs/cart/username/" + drugsId + "/" + username, Cart.class);
+    int x = cart.getDrugsQty();
+
+    Drugs drugs1 = this.restTemplate.getForObject("http://DRUGS-MICROSERVICE/drugs/" + drugsId, Drugs.class);
+    int y = drugs1.getStockQty();
+
+    int z = y - x;
+
+    drugs.setStockQty(z);
+
+    this.drugsRepository.save(drugs);
+
+    return drugs;
+
+  }
+
+  public Drugs updateDrugsDetails(Drugs drugs, String drugsId) throws CustomException, Exception {
+
+    Drugs storingVar = new Drugs();
+    storingVar = this.drugsRepository.save(drugs);
+
+    return storingVar;
+  }
+
+  public String deleteDrugs(String drugsId) {
+
+    this.drugsRepository.deleteById(drugsId);
+    return "Deleted Successfully";
+  }
 
 }
